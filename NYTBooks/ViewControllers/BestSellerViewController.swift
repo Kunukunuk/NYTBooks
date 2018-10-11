2db83e786a145a1a2c9bd7b6446cef9461c5626e
Kun Huang <kkun.huang@gmail.com>
2018-10-11 12:01:43 -0400

    saving last sort by

--- NYTBooks/ViewControllers/BestSellerViewController.swift
+++ NYTBooks/ViewControllers/BestSellerViewController.swift
@@ -13,7 +13,10 @@ class BestSellerViewController: UIViewController, UITableViewDataSource, UITable
     
     var category: Category!
     var bestSeller: [BestSellerBooks] = []
-    var defaultBestSeller: [BestSellerBooks] = []
+    let defaultSort = UserDefaults.standard
+    let sortKey = "lastSort"
+    
+    @IBOutlet weak var segmentSort: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -23,35 +26,49 @@ class BestSellerViewController: UIViewController, UITableViewDataSource, UITable
         tableView.delegate = self
         
         self.title = category.displayName
-        // Do any additional setup after loading the view.
+        
         fetchBestSellerBooks()
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        if let lastSort = defaultSort.object(forKey: sortKey) {
+            let lastIndex = lastSort as! Int
+            segmentSort.selectedSegmentIndex = lastIndex
+            sortBooks(selectedIndex: lastIndex)
+        }
+    }
 
     func fetchBestSellerBooks() {
       
         NYTApiClient().getBestSellerBooks(categoryName: category.listNameEncoded) { (books: [BestSellerBooks]?, error: Error?) in
             if let books = books {
                 self.bestSeller = books
-                self.defaultBestSeller = books
                 self.tableView.reloadData()
             }
         }
     }
     
-    @IBAction func sortBy(_ sender: UISegmentedControl) {
+    func sortBooks(selectedIndex: Int) {
         
-       if (sender.selectedSegmentIndex == 0) {
+        if (selectedIndex == 0) {
             
             bestSeller.sort(by: {$0.rank < $1.rank} )
             tableView.reloadData()
             
-        } else if (sender.selectedSegmentIndex == 1) {
+        } else if (selectedIndex == 1) {
             
             bestSeller.sort(by: { $0.weekOnList > $1.weekOnList })
             tableView.reloadData()
             
         }
+    }
+    
+    @IBAction func sortBy(_ sender: UISegmentedControl) {
+        
+        defaultSort.set(sender.selectedSegmentIndex, forKey: sortKey)
+        sortBooks(selectedIndex: sender.selectedSegmentIndex)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
